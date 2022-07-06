264422442c849edcc19e06cfba5d0541a02dd04a
LlekoX <49719390+LlekoX@users.noreply.github.com>
2022-07-06 09:14:41 +0200

    Add files via upload

---
+++ RecipeWebApp/main.js
@@ -0,0 +1,40 @@
+const searchForm = document.querySelector('form');
+const searchResultDiv = document.querySelector('.search-result');
+const container = document.querySelector('.container');
+let searchQuery = '';
+const APP_ID = '754d886c';
+const APP_key = 'e843ebdfea4413536d8e0086fad440ec';
+//const baseUrl = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_key}`;
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchQuery = e.target.querySelector('input').value;
+    fetchAPI();
+});
+
+async function fetchAPI() {
+    const baseUrl = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&to=21`;
+    const response = await fetch(baseUrl);
+    const data = await response.json();
+    generateHTML(data.hits);
+    console.log(data);
+}
+
+function generateHTML(results) {
+    let generatedHTML = '';
+
+    results.map(result => {
+        generatedHTML += 
+        `
+        <div class="item">
+            <img src="${result.recipe.image}" alt="">
+            <div class="flex-container">
+                <h1 class="title">${result.recipe.label}</h1>
+                <a class="view-button" href="${result.recipe.url}" target="_blank">View Recipe</a>
+            </div>
+            <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
+        </div>
+        `
+    })
+    searchResultDiv.innerHTML = generatedHTML;
+}
